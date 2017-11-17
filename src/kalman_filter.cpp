908c75c0f818dcf5a5dc2b1e2f3abcd8589ae6cd
Subbu Rao <tech@subbu.me>
2017-11-17 00:41:34 -0800

    Commit of code

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,6 @@
 #include "kalman_filter.h"
-
+#include <iostream>
+using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -21,22 +22,52 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  VectorXd y(3);
+  float range = sqrt(x_[0]*x_[0]+x_[1]*x_[1]);
+  y[0] = z[0]-range;
+  y[1] = z[1]-atan2(x_[1],x_[0]);
+  while(y[1]>M_PI){
+    y[1] -= 2*M_PI;
+  }
+  while(y[1]< -M_PI){
+    y[1] += 2*M_PI;
+  }
+  y[2] = z[2]-(x_[0]*x_[2]+x_[1]*x_[3])/range;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  
+  //new estimate
+  x_ = x_ + (K * y);
+  /*cout << " K = "<<K<<endl;
+  cout << "x_ = "<<x_<<endl;
+  cout <<"y = "<<y<<endl;
+  exit(0);*/
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
